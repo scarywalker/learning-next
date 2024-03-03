e80c9cc061cc17ad3911204ff9e4f2fca1baebbf
Gabriel Dischon <gabrielgdischon@gmail.com>
2024-03-04 00:07:11 +0200

    active links

--- 1/my-app/src/app/(auth)/(with-auth-layout)/layout.tsx
+++ 1/my-app/src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,12 +1,34 @@
-export default function ProductDetailsLayout({
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import "./styles.css";
+
+const navLinks = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot Password", href: "/forgot-password" },
+];
+
+export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathName = usePathname();
   return (
-    <>
-      <h2>inner layout</h2>
-      {children}
-    </>
+    <div>
+      {navLinks.map((link) => {
+        const isActive = pathName.startsWith(link.href);
+        return (
+          <Link
+            href={link.href}
+            key={link.name}
+            className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+    </div>
   );
 }
